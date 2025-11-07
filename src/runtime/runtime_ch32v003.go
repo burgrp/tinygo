ff43d6a841b2cdd0379ddf89232d1b3d5bb41554
Pavel Burgr <pb@drake.cz>
2025-11-08 00:11:51 +0100

    WIP

--- src/runtime/runtime_ch32v003.go
+++ src/runtime/runtime_ch32v003.go
@@ -12,55 +12,43 @@ const sysTickInterruptCode = 12
 
 //export main
 func main() {
-	// Route traps to the TinyGo interrupt handler.
-	//riscv.MTVEC.Set(uintptr(unsafe.Pointer(&handleInterruptASM)))
-	//riscv.MSTATUS.SetBits(riscv.MSTATUS_MIE)
+
+	preinit()
 
 	machine.PD0.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	machine.PD4.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
 	machine.PD0.Set(true)
 
+	initSysTick()
+
+	riscv.MSTATUS.SetBits(riscv.MSTATUS_MIE)
 	for {
-		riscv.Asm("ecall")
+		riscv.Asm("wfi")
 	}
 
-	// preinit()
-	// initSysTick()
 	// //machine.InitSerial()
 	// run()
 	// exit(0)
 }
 
+// System tick timer initialization.
+// Sets up SysTick to tick every 100 millisecond.
 func initSysTick() {
 
-	wch.PFIC.STK_CTLR.Set(0)
-	wch.PFIC.STK_SR.Set(0)
-	wch.PFIC.SetSTK_CNTL(0)
-
-	// ticksPerMillisecond := machine.CPUFrequency / 1000
-	// if ticksPerMillisecond == 0 {
-	// 	ticksPerMillisecond = 1
-	// }
-	ticksPerMillisecond := 100
+	// CH32V003 runs at 24 MHz with AHB prescaler of 3.
+	// The datasheet says the AHB prescaler is 1, but that is incorrect.
+	ticksPerMillisecond := 8e6 / 1000
+	if ticksPerMillisecond == 0 {
+		ticksPerMillisecond = 1
+	}
 	wch.PFIC.SetSTK_CMPLR(uint32(ticksPerMillisecond - 1))
-
-	wch.PFIC.SetSTK_CTLR_STRE(1)
-	wch.PFIC.SetSTK_CTLR_STCLK(1)
-	wch.PFIC.SetSTK_CTLR_STIE(1)
-	wch.PFIC.SetSTK_CTLR_INIT(1)
-
-	wch.PFIC.SetIENR1_INTEN12(1)
-	wch.PFIC.IENR1.Set(0xFFFFFFFF)
-	wch.PFIC.IENR2.Set(0xFFFFFFFF)
-
-	wch.PFIC.SetSTK_CTLR_STE(1)
 
 	systickMillis = 0
 
-	//riscv.Asm("ecall")
-	wch.PFIC.SetSTK_CTLR_SWIE(1)
+	wch.PFIC.STK_CTLR.Set(wch.PFIC_STK_CTLR_STE | wch.PFIC_STK_CTLR_STIE | wch.PFIC_STK_CTLR_STCLK | wch.PFIC_STK_CTLR_STRE)
 
+	wch.PFIC.SetIENR1_INTEN12(1)
 }
 
 //go:extern handleInterruptASM
@@ -69,30 +57,27 @@ var handleInterruptASM [0]uintptr
 //export handleInterrupt
 func handleInterrupt() {
 
-	machine.PD4.Set(true)
 	machine.PD4.Set(false)
+	machine.PD4.Set(true)
 
-	// for {
-	// }
-
-	// cause := riscv.MCAUSE.Get()
-	// if cause&(1<<31) != 0 {
-	// 	switch cause & 0xff {
-	// 	case riscv.MachineTimerInterrupt, sysTickInterruptCode:
-	// 		sysTickInterrupt()
-	// 	default:
-	// 		// Unhandled interrupt.
-	// 	}
-	// } else {
-	// 	handleException(uint32(cause))
-	// }
-	// // Clear MCAUSE so interrupt.In() reports the correct state.
-	// riscv.MCAUSE.Set(0)
+	cause := riscv.MCAUSE.Get()
+	if cause&(1<<31) != 0 {
+		switch cause & 0xff {
+		case riscv.MachineTimerInterrupt, sysTickInterruptCode:
+			sysTickInterrupt()
+		default:
+			// Unhandled interrupt.
+		}
+	} else {
+		handleException(uint32(cause))
+	}
+	// Clear MCAUSE so interrupt.In() reports the correct state.
+	riscv.MCAUSE.Set(0)
 }
 
 func handleException(code uint32) {
 
-	machine.PD4.Set(true)
+	//machine.PD4.Set(true)
 
 	print("fatal error: exception with mcause=")
 	print(code)
